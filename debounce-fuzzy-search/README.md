4e329e32294b4d379d4bdfd883a58c270f729026
Max Xue <maxxue1512@gmail.com>
2023-01-10 00:15:57 +1100

    added border to left and top on draggable area

--- debounce-fuzzy-search/README.md
+++ debounce-fuzzy-search/README.md
@@ -4,7 +4,7 @@ https://linear.app/maxxueprojects/team/PRO/active
 https://dribbble.com/shots/18801349-Command-K-menu-Untitled-UI
 https://dribbble.com/tags/command_k
 https://www.google.com/search?q=best+command+k+bar+ui&client=firefox-b-d&sxsrf=ALiCzsaOFsPF3P8m-qdFHYpH1Q8hfZQvIA:1671528394355&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjmuLXB8If8AhWqwTgGHQe4B_oQ_AUoAXoECCMQAw&biw=2560&bih=1327&dpr=1#imgrc=YvQ7qWt7yh75OM
-
+https://cmdk.paco.me/
 
 debounce
 
--- draggable-dialog/src/components/modal/Modal.tsx
+++ draggable-dialog/src/components/modal/Modal.tsx
@@ -8,23 +8,62 @@ interface Point {
 
 export const Modal = () => {
     const initMousePosition = useRef<Point | null>(null);
+    const currTranslatePosition = useRef<Point | null>(null);
     const [currentTranslate, setCurrentTranslate] = useState<Point>({x: 0, y: 0})
+
+    const nodeRef = useRef<HTMLDivElement | null>(null);
 
     const handleOnMouseDown = (e: MouseEvent) => {
         initMousePosition.current = {
             x: e.clientX,
             y: e.clientY
         }
+        currTranslatePosition.current = {
+            x: e.clientX,
+            y: e.clientY
+        }
+        // console.log(nodeRef);
         document.addEventListener('mousemove', handleOnMouseMove);
         document.addEventListener('mouseup', handleOnMouseUp);
     }
 
     const handleOnMouseMove = (e: MouseEvent) => {
-        if (initMousePosition.current && e.clientX > 0 && e.clientY > 0) {
-            const newX = currentTranslate.x + e.clientX - initMousePosition.current.x;
-            const newY = currentTranslate.y + e.clientY - initMousePosition.current.y;
-            setCurrentTranslate({x: newX, y: newY});
+        let newX = currentTranslate.x;
+        let newY = currentTranslate.y;
+
+        if (initMousePosition.current) {
+            newX = currentTranslate.x + e.clientX - initMousePosition.current.x;
         }
+
+        if (initMousePosition.current) {
+            newY = currentTranslate.y + e.clientY - initMousePosition.current.y;
+        }
+
+        if (nodeRef.current?.getBoundingClientRect() && initMousePosition.current) {
+            const leftPos = nodeRef.current?.getBoundingClientRect().left;
+            const topPos = nodeRef.current?.getBoundingClientRect().top;
+
+            // // console.log(topPos);
+            // console.log(leftPos);
+            // if (currTranslatePosition.current)
+            //     console.log(currTranslatePosition.current.x);
+
+
+            if (leftPos <= 0 && currTranslatePosition.current && newX < currTranslatePosition.current.x) {
+                newX = currentTranslate.x;
+            }
+
+            if (topPos <= 0 && currTranslatePosition.current && newY < currTranslatePosition.current.y) {
+                newY = currentTranslate.y;
+            }
+
+        }
+
+        currTranslatePosition.current = {
+            x: newX,
+            y: newY
+        }
+        setCurrentTranslate({x: newX, y: newY});
     }
 
     const handleOnMouseUp = (e: MouseEvent) => {
@@ -33,6 +72,13 @@ export const Modal = () => {
     }
 
     return (
-        <div className='modal-container' onMouseDown={handleOnMouseDown} style={{transform: `translate(${currentTranslate.x}px, ${currentTranslate.y}px)`}}>Modal</div>
+        <div 
+            className='modal-container' 
+            onMouseDown={handleOnMouseDown} 
+            style={{transform: `translate(${currentTranslate.x}px, ${currentTranslate.y}px)`}}
+            ref={nodeRef}
+        >
+            Modal
+        </div>
     )
 }
